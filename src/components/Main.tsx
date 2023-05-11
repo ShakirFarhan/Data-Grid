2b25d73a1a3a975e55fbeea16bfd3ee732fa995b
Shakir Farhan <shakirfarhan123@gmail.com>
2023-05-11 11:48:06 +0530

    changes

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -2,9 +2,9 @@ import React, { useState } from 'react';
 import HotTable from '@handsontable/react';
 import './Main.css';
 interface datatype {
-  id?: number;
+  id?: number | string;
   name?: string;
-  phone?: number;
+  phone?: number | string;
 }
 
 const Main: React.FC = () => {
@@ -25,6 +25,23 @@ const Main: React.FC = () => {
       phone: 6545,
     },
   ]);
+  const [colHeader, setColHeaders] = useState([
+    {
+      title: 'Column 1',
+      type: 'string',
+      id: 'col1',
+    },
+    {
+      title: 'Column 2',
+      type: 'numeric',
+      id: 'col2',
+    },
+    {
+      title: 'Column 3',
+      type: 'string',
+      id: 'col3',
+    },
+  ]);
   const handleDataChange = (changes: any[], source: string) => {
     console.log(changes);
     if (source === 'edit') {
@@ -38,39 +55,56 @@ const Main: React.FC = () => {
       setTableData(newData);
     }
   };
-  console.log(tableData);
   const columnHeaders = (col: number) => {
-    if (col === 0) {
-      return `
-      <div class="col-block">
-      <h6 class="col-header">Column 1</h6>
- <span class="col-type">string</span>
-      </div>`;
-    } else if (col === 1) {
-      return ` <div class="col-block">
-      <h6 class="col-header">Column 1</h6>
- <span class="col-type">string</span>
-      </div>`;
-    } else {
-      return ` <div class="col-block">
-      <h6 class="col-header">Column 1</h6>
- <span class="col-type">string</span>
-      </div>`;
-    }
+    const headerHtml = colHeader
+      .map((text, index) => {
+        if (index === col) {
+          return `
+          <div class="col-block">
+            <h6 class="col-header">${text.title}</h6>
+            <span class="col-type">${text.type}</span>
+          </div>
+        `;
+        } else {
+          return '';
+        }
+      })
+      .join('');
+
+    return headerHtml;
+  };
+  const handleAddColumn = () => {
+    const newColHeaders: {
+      title: string;
+      type: string;
+      id: string;
+    }[] = [...colHeader, { title: 'column3', type: '', id: '' }];
+    setColHeaders(newColHeaders);
+    setTableData([...tableData]);
+  };
+  const handleAddRule = () => {
+    setTableData([...tableData, { id: '', name: '', phone: '' }]);
   };
 
   return (
-    <HotTable
-      data={tableData}
-      stretchH="all"
-      colHeaders={columnHeaders}
-      // columns={columns}
-      rowHeaders={false}
-      // colHeaders={false}
-      className="custom-table"
-      afterChange={handleDataChange}
-    />
+    <>
+      <HotTable
+        data={tableData}
+        stretchH="all"
+        colHeaders={columnHeaders}
+        rowHeaders={false}
+        className="custom-table"
+        afterChange={handleDataChange}
+      />
+      <button onClick={handleAddColumn} className="add">
+        Add Column
+      </button>
+      <button onClick={handleAddRule} className="add">
+        Add Rule
+      </button>
+    </>
   );
 };
 
 export default Main;
+//
